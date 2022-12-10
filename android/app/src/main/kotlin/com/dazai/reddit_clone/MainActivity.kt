8f2fded6c790902473f1e0589fc20c5387130832
Nikhil <nik.05.jindal@gmail.com>
2022-12-10 19:24:42 +0530

    Project setup and palette added

---
+++ android/app/src/main/kotlin/com/dazai/reddit_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dazai.reddit_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/theme/palette.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reddit Clone',
+      theme: Palette.darkModeAppTheme,
+      home: const LoginScreen(),
+    );
+  }
+}
