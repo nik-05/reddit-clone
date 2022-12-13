209e6c19682ecd119fcefa6216ed1120d676d02d
Nikhil <nik.05.jindal@gmail.com>
2022-12-13 14:56:19 +0530

    Creating community screen ui

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -6,13 +6,62 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CreateCommunityScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _CreateCommunityScreenState();
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final TextEditingController communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    communityNameController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create a community'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Align(
+                alignment: Alignment.topLeft,
+                child: Text('Community name')),
+            const SizedBox(height: 10),
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                hintText: 'r/community_name',
+                filled: true,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('Create community', style: TextStyle(
+                fontSize: 17,
+              ),),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
